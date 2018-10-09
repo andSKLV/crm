7022ddcd4b66f37fd1581892d03c68cbdb799da9
Andrei Sokolov <xlloll3@gmail.com>
2018-10-09 15:39:51 +0300

    + profile dashboard added

--- js/controllers/profileCtrl.js
+++ js/controllers/profileCtrl.js
@@ -1,16 +1,121 @@
+import Company from '../protos/company.js';
+
 app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location, myFactory) {
-  this.myFactory = myFactory;
+  $scope.myFactory = myFactory;
   const scope = this;
+  debugger;
   init();
 
-  function init () {
-    $http.post('src/profile-dashboard.json').then((resp)=>{
-      $scope.currObj = resp.data;
-      debugger;
-    },(err)=>{
-      console.error(err);
-    })
+  async function init () {
+    await loadDash();
+    await loadComp();
+
+    function loadDash () {
+      return $http.post('src/profile-dashboard.json').then((resp)=>{
+        $scope.currObj = resp.data;
+        debugger;
+      },(err)=>{
+        console.error(err);
+      })
+    }
+    function loadComp() {
+      scope.loadCompany('650');
+    }
   }
+
+  this.loadCompany = function (id) {
+    // const myFactory = $scope.myFactory;
+    const data = {};
+    data.type = 'load_company';
+    data.id=id;
+    return $http.post('php/search.php', data).then(async (resp) => {
+        const data = resp.data;
+        myFactory.newClientCard = generateClientCard(data);
+        const companyObj = new Company();
+        myFactory.companyObj = companyObj;
+        companyObj.parseFromCompaniesResponse(data) //создаем объект с  id  из ответа и сохраняем ответ внутри
+        companyObj.card = myFactory.newClientCard;
+        companyObj.markAsLoaded();
+        clearSearch();
+        /**
+         *  Функция генерации объекта карточки клиента из данных из БД
+         * @param {obj} data - ответ из БД
+         * @returns {obj} - объект карточки клиента
+         */
+        function generateClientCard (data) {
+            return {
+                'Данные компании':
+                {
+                   "Форма организации": getOrgForm(data.OrganizationFormID),
+                   "Наименование организации": data.name,
+                   "Дата регистрации": getDate(data.registration_date),
+                   "Наименование рег. органа": data.who_registrate,
+                 },
+                 "Генеральный директор":
+                 {
+                   "ФИО директора":data.director_name,
+                   "Серия и номер паспорта":data.general_director_passport,
+                   "Когда выдан":getDate(data.give_date),
+                   "Кем выдан":data.director_authority,
+                 },
+                 "Реквизиты компании":
+                 {
+                   "ОГРН":data.OGRN,
+                   "ИНН/КПП": getInnKpp(data),
+                   "ОКПО":data.OKPO,
+                   "ОКВЭД":data.OKVED,
+                 },
+                 "Банковские реквизиты":
+                 {
+                   "р/счет":data.r_account,
+                   "к/счет":data.k_account,
+                   "Банк":data.bank,
+                   "БИК":data.bik,
+                 }
+               }
+        }
+        /**
+         * Функция возвращает наименование формы компании 
+         * @param {number} id 
+         */
+        function getOrgForm (id) {
+            if (id==='0') return '';
+            const forms = {
+                1: "ЗАО",
+                2: "ООО",
+                3: "ОАО",
+                4: "ИП"
+            }
+            return forms[+id];
+        }
+        /**
+         * Function to parse INN and KPP from loaded obj
+         * @param {obj} data object of client from DB
+         */
+        function getInnKpp (data) {
+            if (data.INN===""&&data.KPP==="") return "";
+            else return `${data.INN} / ${data.KPP}`;
+        }
+        function getDate (date) {
+            return (date==='0000-00-00') ? '' : date; 
+        } 
+    },function error(resp){
+        console.error(resp);
+    })
+}
+/**
+ * Deleting serach result after choosing one of the results
+ */
+function clearSearch () {
+    try {
+        $rootScope.search_result = [];
+    }
+    catch (err) {
+        console.error (`Clear search results problem: ${err}`);
+    }
+}
+
+
   $scope.newDashboard = {
     currentPage: 0,
     previousPage: -1,
