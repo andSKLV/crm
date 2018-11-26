f0e47f949d8a3520799f489bb2b1062108119b88
Andrei Sokolov <xlloll3@gmail.com>
2018-11-26 11:09:23 +0300

    -refactoring

--- js/controllers/profileCtrl.js
+++ js/controllers/profileCtrl.js
@@ -2,11 +2,11 @@ import Company from '../protos/company.js';
 import Profile from '../protos/profile.js';
 import Loading from '../protos/loading.js';
 
-app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location, myFactory) {
+app.controller('profileCtrl', function ($scope, $rootScope, $http, $q, $location, myFactory) {
   $scope.myFactory = myFactory;
   const scope = this;
-  
-  async function init () {
+
+  async function init() {
     const id = $scope.myFactory.companyObj.id;
     if (!id) {
       alert('Ошибка открытия клиента. Пожалуйста обратиетсь к разработчику');
@@ -19,15 +19,15 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
     modal.show();
     await $scope.loadCompany(id);
     pr.store.calcLinks = await $scope.loadCalcLinks(id);
-    const calcs =  await $scope.loadCalculations(pr.store.calcLinks);//загрузка расчетов
+    const calcs = await $scope.loadCalculations(pr.store.calcLinks);//загрузка расчетов
     pr.store.calculations = fixPremia(calcs);
     // TODO: линки с БД connections
     modal.hide();
 
-    function loadDash () {
-      return $http.post('./src/profile-dashboard.json').then((resp)=>{
+    function loadDash() {
+      return $http.post('./src/profile-dashboard.json').then((resp) => {
         $scope.currObj = resp.data;
-      },(err)=>{
+      }, (err) => {
         console.error(err);
       })
     }
@@ -35,15 +35,15 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
      * Функция изменения вида значения фактической премии, так как она может быть записана с коэфициентом
      * @param {Array} arr - массив с расчетами из БД
      */
-    function fixPremia(arr){
+    function fixPremia(arr) {
       if (!arr) return [];
-      const fixed = arr.map(calc=>{
+      const fixed = arr.map(calc => {
         const oldVal = calc['fact_premia'];
         let newVal = null;
-        if (!oldVal||oldVal===';1') newVal = calc['total_price'];
+        if (!oldVal || oldVal === ';1') newVal = calc['total_price'];
         else {
           const ind = oldVal.search(';');
-          if (ind>=1) newVal = oldVal.slice(0,ind);
+          if (ind >= 1) newVal = oldVal.slice(0, ind);
         }
         calc['fact_premia'] = newVal;
         return calc;
@@ -60,11 +60,11 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
     query.type = 'company_calculations';
     query.model = 'company_id';
     query.id = id;
-    return $http.post('php/load.php',query).then(resp=>{
+    return $http.post('php/load.php', query).then(resp => {
       if (Array.isArray(resp.data)) {
         const was = {};
         // выбираем только уникальные айдишники, без повторов
-        const uniq = resp.data.filter(link=>{
+        const uniq = resp.data.filter(link => {
           if (!was[link.calc_id]) {
             was[link.calc_id] = true;
             return true;
@@ -74,9 +74,9 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
         //переворачиваем для хронологического порядка
         uniq.reverse();
         return uniq;
-      } 
-      else alert ('Возникли проблемы с загрузкой привязанных расчетов. Обратитесь к разработчику');
-    },err=>{
+      }
+      else alert('Возникли проблемы с загрузкой привязанных расчетов. Обратитесь к разработчику');
+    }, err => {
       console.error(err);
     });
   }
@@ -88,93 +88,93 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
     // const myFactory = $scope.myFactory;
     const data = {};
     data.type = 'load_company';
-    data.id=id;
+    data.id = id;
     return $http.post('php/search.php', data).then(async (resp) => {
-        const data = resp.data;
-        myFactory.newClientCard = generateClientCard(data);
-        const companyObj = new Company();
-        myFactory.companyObj = companyObj;
-        companyObj.parseFromCompaniesResponse(data) //создаем объект с  id  из ответа и сохраняем ответ внутри
-        companyObj.card = myFactory.newClientCard;
-        companyObj.markAsLoaded();
-        clearSearch();
-        /**
-         *  Функция генерации объекта карточки клиента из данных из БД
-         * @param {obj} data - ответ из БД
-         * @returns {obj} - объект карточки клиента
-         */
-        function generateClientCard (data) {
-            return {
-              'Данные компании':
-              {
-                 "Форма организации": getOrgForm(data.OrganizationFormID),
-                 "Наименование организации": data.name,
-                 "Дата регистрации": getDate(data.registration_date),
-                 "Наименование рег. органа": data.who_registrate,
-               },
-               "Генеральный директор":
-               {
-                 "ФИО директора":data.director_name,
-                 "Серия и номер паспорта":data.general_director_passport,
-                 "Когда выдан":getDate(data.give_date),
-                 "Кем выдан":data.director_authority,
-               },
-               "Продолжение": 
-               {
-                  "Место рождения": "",
-                  "Адрес регистрации": "",
-               },
-               "Реквизиты компании":
-               {
-                 "ОГРН":data.OGRN,
-                 "ИНН": data.INN,
-                 "КПП": data.KPP,
-                 "ОКПО":data.OKPO,
-                 "ОКВЭД":data.OKVED,
-               },
-               "Банковские реквизиты":
-               {
-                 "р/счет":data.r_account,
-                 "к/счет":data.k_account,
-                 "Банк":data.bank,
-                 "БИК":data.bik,
-               },
-               "Доп. информация":
-               {
-                  "Телефон":data.company_phone,
-                  "Эл. почта":data.company_mail,
-                  "Юридический адрес":data.Legal_address,
-                  "Фактический адрес":data.Real_address,
-               }
-               }
-        }
-        /**
-         * Функция возвращает наименование формы компании 
-         * @param {number} id 
-         */
-        function getOrgForm (id) {
-            if (id==='0') return '';
-            const forms = {
-                1: "ЗАО",
-                2: "ООО",
-                3: "ОАО",
-                4: "ИП"
+      const data = resp.data;
+      myFactory.newClientCard = generateClientCard(data);
+      const companyObj = new Company();
+      myFactory.companyObj = companyObj;
+      companyObj.parseFromCompaniesResponse(data) //создаем объект с  id  из ответа и сохраняем ответ внутри
+      companyObj.card = myFactory.newClientCard;
+      companyObj.markAsLoaded();
+      clearSearch();
+      /**
+       *  Функция генерации объекта карточки клиента из данных из БД
+       * @param {obj} data - ответ из БД
+       * @returns {obj} - объект карточки клиента
+       */
+      function generateClientCard(data) {
+        return {
+          'Данные компании':
+            {
+              "Форма организации": getOrgForm(data.OrganizationFormID),
+              "Наименование организации": data.name,
+              "Дата регистрации": getDate(data.registration_date),
+              "Наименование рег. органа": data.who_registrate,
+            },
+          "Генеральный директор":
+            {
+              "ФИО директора": data.director_name,
+              "Серия и номер паспорта": data.general_director_passport,
+              "Когда выдан": getDate(data.give_date),
+              "Кем выдан": data.director_authority,
+            },
+          "Продолжение":
+            {
+              "Место рождения": "",
+              "Адрес регистрации": "",
+            },
+          "Реквизиты компании":
+            {
+              "ОГРН": data.OGRN,
+              "ИНН": data.INN,
+              "КПП": data.KPP,
+              "ОКПО": data.OKPO,
+              "ОКВЭД": data.OKVED,
+            },
+          "Банковские реквизиты":
+            {
+              "р/счет": data.r_account,
+              "к/счет": data.k_account,
+              "Банк": data.bank,
+              "БИК": data.bik,
+            },
+          "Доп. информация":
+            {
+              "Телефон": data.company_phone,
+              "Эл. почта": data.company_mail,
+              "Юридический адрес": data.Legal_address,
+              "Фактический адрес": data.Real_address,
             }
-            return forms[+id];
         }
-        /**
-         * Function to parse INN and KPP from loaded obj
-         * @param {obj} data object of client from DB
-         */
-        function getInnKpp (data) {
-            if (data.INN===""&&data.KPP==="") return "";
-            else return `${data.INN} / ${data.KPP}`;
+      }
+      /**
+       * Функция возвращает наименование формы компании 
+       * @param {number} id 
+       */
+      function getOrgForm(id) {
+        if (id === '0') return '';
+        const forms = {
+          1: "ЗАО",
+          2: "ООО",
+          3: "ОАО",
+          4: "ИП"
         }
-        function getDate (date) {
-            return (date==='0000-00-00') ? '' : date; 
-        } 
-    },function error(resp){
-        console.error(resp);
+        return forms[+id];
+      }
+      /**
+       * Function to parse INN and KPP from loaded obj
+       * @param {obj} data object of client from DB
+       */
+      function getInnKpp(data) {
+        if (data.INN === "" && data.KPP === "") return "";
+        else return `${data.INN} / ${data.KPP}`;
+      }
+      function getDate(date) {
+        return (date === '0000-00-00') ? '' : date;
+      }
+    }, function error(resp) {
+      console.error(resp);
     })
   }
   /**
@@ -187,23 +187,23 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
       console.error(`Ошибка формата:${typeof links} должкен быть array`);
       return false;
     }
-    if (links.length===0) {
+    if (links.length === 0) {
       console.log('Нет расчетов');
       return false;
     }
     const query = {};
-    query.type='load_linked_calcs';
-    query.ids = links.map(link=>link['calc_id']);
-    return $http.post('php/load.php',query).then(resp=>{
+    query.type = 'load_linked_calcs';
+    query.ids = links.map(link => link['calc_id']);
+    return $http.post('php/load.php', query).then(resp => {
       if (!Array.isArray(resp.data)) {
-        alert (`Проблема с загрузкой расчетов. Пожалуйста, по возможности не закрывайте это окно и обратитесь к разработчику`);
+        alert(`Проблема с загрузкой расчетов. Пожалуйста, по возможности не закрывайте это окно и обратитесь к разработчику`);
         console.error(resp.data);
         return null;
       }
-      if (resp.data.length!==links.length) console.warn(`Размер полученных данных не совпадает с запрошенными`);
+      if (resp.data.length !== links.length) console.warn(`Размер полученных данных не совпадает с запрошенными`);
       return resp.data;
 
-    },err=>{
+    }, err => {
       console.error(err);
     })
   }
@@ -254,7 +254,7 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
   };
   $scope.isntEmpty = (obj) => {
     for (let key in obj) {
-        if (obj[key] != "" && obj[key] != "Форма организации" && obj[key] != undefined) return true;
+      if (obj[key] != "" && obj[key] != "Форма организации" && obj[key] != undefined) return true;
     }
     return false;
   };
@@ -265,31 +265,31 @@ app.controller('profileCtrl', function ($scope,$rootScope, $http, $q, $location,
     };
     $location.path(path);
   };
-    /**
-   * Deleting serach result after choosing one of the results
-   */
-  function clearSearch () {
+  /**
+ * Deleting serach result after choosing one of the results
+ */
+  function clearSearch() {
     try {
-        $rootScope.search_result = [];
+      $rootScope.search_result = [];
     }
     catch (err) {
-        console.error (`Clear search results problem: ${err}`);
+      console.error(`Clear search results problem: ${err}`);
     }
   }
   $scope.$on('$destroy', function () {
-    $scope.myFactory.removeCellSelection ('dashboard_container',true);
+    $scope.myFactory.removeCellSelection('dashboard_container', true);
   });
   /**
    * для вывода подсказок
    */
-  $scope.tooltip={
-    title:"",
-    fadeIn(title){
-      if (title==='') title = 'Без названия';
-      this.title=title;
+  $scope.tooltip = {
+    title: "",
+    fadeIn(title) {
+      if (title === '') title = 'Без названия';
+      this.title = title;
     },
-    fadeOut(){
-      this.title='';
+    fadeOut() {
+      this.title = '';
     }
   };
 
